81dce24f1f48eb88ca077bbd5b6f19677991dcb8
Jeong Seon U <jung48273@gmail.com>
2023-04-06 22:44:34 +0900

    feat: add screenSize config func to App.vue

--- nonata/src/App.vue
+++ nonata/src/App.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+const setScreenSize = function () {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+setScreenSize();
+window.addEventListener("resize", setScreenSize);
+</script>
 
 <template></template>
 
