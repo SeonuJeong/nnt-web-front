1d5e37c73f7d3000f8676491829e33ae44d657b0
Jeong Seon U <jung48273@gmail.com>
2023-04-06 22:46:06 +0900

    feat: add App.vue basic basic template and style

--- nonata/src/App.vue
+++ nonata/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { RouterView } from "vue-router";
+
 const setScreenSize = function () {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -7,6 +9,20 @@ setScreenSize();
 window.addEventListener("resize", setScreenSize);
 </script>
 
-<template></template>
+<template>
+  <RouterView class="module area_sidebar" />
+</template>
 
-<style scoped></style>
+<style scoped>
+.module {
+  margin: 0 auto;
+  max-width: 480px;
+  width: 100%;
+  position: relative;
+  max-height: 1000px;
+}
+
+.area_sidebar {
+  height: calc(var(--vh, 1vh) * 100);
+}
+</style>
